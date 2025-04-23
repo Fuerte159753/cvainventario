57cba3866a66d1354189fc9048bd077cbde64a82
Alejavi568 <122804403+Fuerte159753@users.noreply.github.com>
2025-04-23 00:33:19 -0600

    Update db.js

--- Backend cva/config/db.js
+++ Backend cva/config/db.js
@@ -1,10 +1,10 @@
 const mysql = require('mysql2/promise');
 
 const pool = mysql.createPool({
-  host: 'sql3.freesqldatabase.com',
-  user: 'sql3773680',
-  password: '6Q5RCgsIsA',
-  database: 'sql3773680',
+  host: 'sql5.freesqldatabase.com',
+  user: 'sql5774850',
+  password: 'Mh2ga2RjLq',
+  database: 'sql5774850',
   port: 3306,
   waitForConnections: true,
   connectionLimit: 10,
